f484a5bd84d62a6ad8e9a8e15e4b43e409e97d30
PedroMSFernandes5 <1231850@isep.ipp.pt>
2024-03-11 20:23:53 +0000

    Fix invalid email issue

--- CA1/basic/src/main/java/com/greglturnquist/payroll/Employee.java
+++ CA1/basic/src/main/java/com/greglturnquist/payroll/Employee.java
@@ -97,6 +97,13 @@ public class Employee {
         this.description = description;
     }
 
+    public boolean isValidEmail(String email) {
+        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
     public int getJobYears() {
         return jobYears;
     }
